43b989381f31ec6a7f98cee447b0f85c306154b1
MinGyu-Kang <kagrin97@gmail.com>
2023-01-30 11:54:20 +0900

    feat/back : 기존 drugList형식에서 달력일기장 형식으로 전환

--- backend/models/calendar.js
+++ backend/models/calendar.js
@@ -3,8 +3,8 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const calendarSchema = new Schema({
-  date: { type: String, required: true },
-  drugList: [{ type: String, required: true }],
+  date: { type: String, required: true, default: Date.now() },
+  detail: [{ type: String, required: true }],
   creator: { type: mongoose.Types.ObjectId, required: true, ref: "User" },
 });
 
