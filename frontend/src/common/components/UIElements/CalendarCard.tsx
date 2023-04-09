f5cfb0d293aba876b18a0bc665e546c7f6ecd76d
MinGyu-Kang <kagrin97@gmail.com>
2023-04-09 12:02:22 +0900

    refactor : CalendarCard.tsx 기존 뷰 코드를 가독성 좋게 삼항연산자를 if문으로 변경

--- frontend/src/common/components/UIElements/CalendarCard.tsx
+++ frontend/src/common/components/UIElements/CalendarCard.tsx
@@ -15,34 +15,39 @@ interface CalendarCardProps {
 }
 
 export default function CalendarCard(props: CalendarCardProps) {
+  const isCardContents = () => {
+    if (props.cardContents) {
+      return (
+        <React.Fragment>
+          <div className="calendar-card__contents">
+            <span>{minusToDot(props.cardContents.createdDate)}</span>
+            <h3>{props.cardContents && props.cardContents.title}</h3>
+          </div>
+          <div className="block-center">
+            <Button size="small" onClick={props.onClick}>
+              상세보기
+            </Button>
+          </div>
+        </React.Fragment>
+      );
+    }
+
+    return (
+      <React.Fragment>
+        <div className="calendar-card__contents">
+          <h3>제목 </h3>
+        </div>
+        <div className="block-center">
+          <Button size="small" onClick={props.onClick}>
+            작성하기
+          </Button>
+        </div>
+      </React.Fragment>
+    );
+  };
   return (
     <div className="calendar-card__container">
-      <Card className="CalendarCard">
-        {props.cardContents ? (
-          <React.Fragment>
-            <div className="calendar-card__contents">
-              <span>{minusToDot(props.cardContents.createdDate)}</span>
-              <h3>{props.cardContents && props.cardContents.title}</h3>
-            </div>
-            <div className="block-center">
-              <Button size="small" onClick={props.onClick}>
-                상세보기
-              </Button>
-            </div>
-          </React.Fragment>
-        ) : (
-          <React.Fragment>
-            <div className="calendar-card__contents">
-              <h3>제목 </h3>
-            </div>
-            <div className="block-center">
-              <Button size="small" onClick={props.onClick}>
-                작성하기
-              </Button>
-            </div>
-          </React.Fragment>
-        )}
-      </Card>
+      <Card className="CalendarCard">{isCardContents()}</Card>
     </div>
   );
 }
