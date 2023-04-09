3bad3a2a2a1bd076557546a08f6dcd1f6c878e11
MinGyu-Kang <kagrin97@gmail.com>
2023-04-09 11:56:31 +0900

    refactor : SignupVIew.tsx 기존 뷰 코드를 가독성 좋게 코드스플릿팅

--- frontend/src/pages/UserAuth/components/Signup/SignupView.tsx
+++ frontend/src/pages/UserAuth/components/Signup/SignupView.tsx
@@ -32,116 +32,140 @@ export default function SignupView({
     getValues,
   } = useForm<FormValue>({ mode: "onChange" });
 
+  const renderEmailForm = () => {
+    return (
+      <div className="form-control__items ">
+        <label htmlFor="email">이메일</label>
+        <input
+          id="email"
+          type="text"
+          placeholder="test@email.com"
+          {...register("email", {
+            required: "이메일은 필수 입력입니다.",
+            pattern: {
+              value:
+                /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i,
+              message: "이메일 형식에 맞지 않습니다.",
+            },
+
+            validate: {
+              check: async (email) => {
+                return checkExistingUser(email);
+              },
+            },
+          })}
+        />
+        {errors.email && <small role="alert">{errors.email.message}</small>}
+      </div>
+    );
+  };
+
+  const renderPassWordForm = () => {
+    return (
+      <div className="form-control__items">
+        <label htmlFor="password">비밀번호</label>
+        <input
+          id="password"
+          type="password"
+          placeholder="*******"
+          {...register("password", {
+            required: "비밀번호는 필수 입력입니다.",
+            minLength: {
+              value: 7,
+              message: "7자리 이상 비밀번호를 입력하세요.",
+            },
+          })}
+        />
+        {errors.password && (
+          <small role="alert">{errors.password.message}</small>
+        )}
+      </div>
+    );
+  };
+
+  const renderPassWordCheckForm = () => {
+    return (
+      <div className="form-control__items">
+        <label htmlFor="password">비밀번호 확인</label>
+        <input
+          id="password"
+          type="password"
+          placeholder="*******"
+          {...register("passwordConfirm", {
+            required: "비밀번호는 필수 입력입니다.",
+            minLength: {
+              value: 7,
+              message: "7자리 이상 비밀번호를 사용하세요.",
+            },
+            validate: {
+              check: (val) => {
+                if (getValues("password") !== val) {
+                  return "비밀번호가 일치하지 않습니다.";
+                }
+              },
+            },
+          })}
+        />
+        {errors.passwordConfirm && (
+          <small role="alert">{errors.passwordConfirm.message}</small>
+        )}
+      </div>
+    );
+  };
+
+  const renderNickNameForm = () => {
+    return (
+      <div className="form-control__items">
+        <label htmlFor="nickName">닉네임</label>
+        <input
+          id="nickName"
+          type="text"
+          placeholder="닉네임을 입력해주세요"
+          {...register("nickName", {
+            required: "닉네임은 필수 입력입니다.",
+            minLength: {
+              value: 2,
+              message: "2자리 이상 닉네임을 사용하세요.",
+            },
+          })}
+        />
+        {errors.nickName && (
+          <small role="alert">{errors.nickName.message}</small>
+        )}
+      </div>
+    );
+  };
+
+  const renderAvatarForm = () => {
+    return (
+      <div className="form-control__items container-img">
+        <label htmlFor="picture">프로필 사진</label>
+        <div className="form-control__items-img center">
+          <img src={imgFile} alt="프로필 이미지" />
+        </div>
+        <input
+          {...register("image")}
+          id="picture"
+          type="file"
+          className="hidden"
+          accept="image/*"
+          onChange={savePreViewFile}
+          ref={imgRef}
+        />
+        <small role="alert">선택하지 않을 시 기본이미지가 적용됩니다.</small>
+      </div>
+    );
+  };
   return (
     <React.Fragment>
       {isLoading && <LoadingSpinner asOverlay />}
       <ErrorModal error={error} onClear={clearError} />
       <form className="form-control" onSubmit={handleSubmit(onSubmitSignup)}>
-        <div className="form-control__items ">
-          <label htmlFor="email">이메일</label>
-          <input
-            id="email"
-            type="text"
-            placeholder="test@email.com"
-            {...register("email", {
-              required: "이메일은 필수 입력입니다.",
-              pattern: {
-                value:
-                  /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i,
-                message: "이메일 형식에 맞지 않습니다.",
-              },
-
-              validate: {
-                check: async (email) => {
-                  return checkExistingUser(email);
-                },
-              },
-            })}
-          />
-          {errors.email && <small role="alert">{errors.email.message}</small>}
-        </div>
-
-        <div className="form-control__items">
-          <label htmlFor="password">비밀번호</label>
-          <input
-            id="password"
-            type="password"
-            placeholder="*******"
-            {...register("password", {
-              required: "비밀번호는 필수 입력입니다.",
-              minLength: {
-                value: 7,
-                message: "7자리 이상 비밀번호를 입력하세요.",
-              },
-            })}
-          />
-          {errors.password && (
-            <small role="alert">{errors.password.message}</small>
-          )}
-        </div>
-
-        <div className="form-control__items">
-          <label htmlFor="password">비밀번호 확인</label>
-          <input
-            id="password"
-            type="password"
-            placeholder="*******"
-            {...register("passwordConfirm", {
-              required: "비밀번호는 필수 입력입니다.",
-              minLength: {
-                value: 7,
-                message: "7자리 이상 비밀번호를 사용하세요.",
-              },
-              validate: {
-                check: (val) => {
-                  if (getValues("password") !== val) {
-                    return "비밀번호가 일치하지 않습니다.";
-                  }
-                },
-              },
-            })}
-          />
-          {errors.passwordConfirm && (
-            <small role="alert">{errors.passwordConfirm.message}</small>
-          )}
-        </div>
-
-        <div className="form-control__items">
-          <label htmlFor="nickName">닉네임</label>
-          <input
-            id="nickName"
-            type="text"
-            placeholder="닉네임을 입력해주세요"
-            {...register("nickName", {
-              required: "닉네임은 필수 입력입니다.",
-              minLength: {
-                value: 2,
-                message: "2자리 이상 닉네임을 사용하세요.",
-              },
-            })}
-          />
-          {errors.nickName && (
-            <small role="alert">{errors.nickName.message}</small>
-          )}
-        </div>
-
-        <div className="form-control__items container-img">
-          <label htmlFor="picture">프로필 사진</label>
-          <div className="form-control__items-img center">
-            <img src={imgFile} alt="프로필 이미지" />
-          </div>
-          <input
-            {...register("image")}
-            id="picture"
-            type="file"
-            className="hidden"
-            accept="image/*"
-            onChange={savePreViewFile}
-            ref={imgRef}
-          />
-          <small role="alert">선택하지 않을 시 기본이미지가 적용됩니다.</small>
-        </div>
-
+        {renderEmailForm()}
+        {renderPassWordForm()}
+        {renderPassWordCheckForm()}
+        {renderNickNameForm()}
+        {renderAvatarForm()}
         <Button type="submit">회원가입</Button>
       </form>
     </React.Fragment>
